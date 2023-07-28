ebc7315597fd7e7b4e07fa52a16ad577b76673e0
aaawwwws <adeqa1@icloud.com>
2023-07-29 00:35:58 +0900

    FF14_calculator

---
+++ main.py
@@ -0,0 +1,76 @@
+from decimal import Decimal
+from typing import List, Dict
+import math
+
+[Main, WD, CRIT, DH, DET, TNC] = [
+    "メインステータス",
+    "ウェポンダメージ",
+    "クリティカル",
+    "ダイレクトヒット",
+    "意志力",
+    "不屈",
+]
+
+
+def INPUT(List):
+    print(f"{List}を入力してください")
+    return input()
+
+
+class Damage_Calc:
+    def __init__(
+        self, Main_Status: int, WP_Damage: int, CRIT: int, DH: int, DET: int, TNC: int
+    ) -> None:
+        # メインステータス(STR)とかの値を取る
+        self.Main_Status: int = int(Main_Status)
+        # ウェポンダメージ
+        self.WP_Damage: int = int(WP_Damage)
+        # サブステの値を取る
+        self.CRIT: int = int(CRIT)
+        self.DH: int = int(DH)
+        self.DET: int = int(DET)
+        self.TNC: int = int(TNC)
+        self.default: dict[str, int] = {
+            "LvMain": int(390),
+            "LvSub": int(400),
+            "LvDiv": int(1900),
+        }
+
+        # クリティカル発生率
+
+    def Crit_Rate(self):
+        Result = Decimal(
+            (200 * (self.CRIT - self.default["LvSub"]) / self.default["LvDiv"] + 50)
+            / 10
+        )
+
+        Result_floor = round(Result, 1)
+        return Result_floor
+
+        # クリティカルダメージ
+
+    def Crit_Damage(self):
+        Result = Decimal(
+            (200 * (self.CRIT - self.default["LvSub"]) / self.default["LvDiv"] + 1400)
+            / 10
+        )
+        Result_floor = round(Result, 1)
+        return Result_floor
+
+        # ダイレクトヒットダメージ
+
+    def DH_Damage(self):
+        Result = Decimal(
+            550 * (self.DH - self.default["LvSub"]) / self.default["LvDiv"] / 10
+        )
+        Result_floor = round(Result, 1)
+        return Result_floor
+
+
+const = Damage_Calc(
+    INPUT(Main), INPUT(WD), INPUT(CRIT), INPUT(DH), INPUT(DET), INPUT(TNC)
+)
+
+print(
+    f"クリティカル発生率{const.Crit_Rate()}%,クリティカル倍率{ const.Crit_Damage()}%ダイレクトヒット発生率{const.DH_Damage()}%"
+)
