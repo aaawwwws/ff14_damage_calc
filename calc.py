4d3f588f6c31c9035814fa9daa08014195ba6865
aaawwwws <adeqa1@icloud.com>
2023-07-31 07:29:33 +0900

    本日の作業前

--- calc.py
+++ calc.py
@@ -1,6 +1,8 @@
 from decimal import Decimal
 from input import Main_sta, Wd_sta, Crit_sta, DH_sta, Det_sta, Tnc_sta
 import math
+from calc import Skill_Damage
+from input import Input
 
 
 class Damage_Calc:
@@ -16,7 +18,6 @@ class Damage_Calc:
         self.DH: int = int(DH)
         self.DET: int = int(DET)
         self.TNC: int = int(TNC)
-        self.Attribute = int(115)
         self.default: dict[str, int] = {
             "LvMain": int(390),
             "LvSub": int(400),
@@ -34,10 +35,8 @@ class Damage_Calc:
 
         # 武器基本性能
 
-    def WD_Damage(self):
-        Result = Decimal(
-            (self.default["LvMain"] * self.Attribute / 1000) + self.WP_Damage
-        )
+    def WD_Damage(self, Attribute):
+        Result = Decimal((self.default["LvMain"] * Attribute / 1000) + self.WP_Damage)
         F_Result = math.floor(Result)
         return F_Result
         # クリティカル発生率
@@ -89,22 +88,23 @@ class Damage_Calc:
         return Result_floor
 
 
+# さらちゃんブログの式を参照してください
 class Skill_Damage(Damage_Calc):
-    def __init__(self, Potency: int, Trait: int) -> None:
+    def __init__(self, Potency: int, Trait: int, Attribute) -> None:
         self.Potency: int = Potency
         self.Status = Damage_Calc(Main_sta, Wd_sta, Crit_sta, DH_sta, Det_sta, Tnc_sta)
         self.ATK: int = int(self.Status.Atack_Rate())
         self.DET: int = int(self.Status.DET_Damage())
-        self.WD: int = int(self.Status.WD_Damage())
+        self.WD: int = int(self.Status.WD_Damage(Attribute))
         self.TNC: int = int(self.Status.TNC_Damage())
         self.Trait: int = int(Trait)
 
-    def D1(self) -> list:
-        D1_Result: float = Decimal(self.Potency * self.ATK * self.DET / 100 / 1000)
+    def D1(self) -> int:
+        D1_Result: int = Decimal(self.Potency * self.ATK * self.DET / 100 / 1000)
         D1_Final_Result: int = math.floor(D1_Result)
         return D1_Final_Result
 
-    def D2(self, D1: float) -> float:
+    def D2(self, D1: int) -> int:
         Result = Decimal(D1 * self.TNC / 1000 * self.WD / 100 * self.Trait / 100)
         D2_Final_Result: int = math.floor(Result)
         return D2_Final_Result
--- main.py
+++ main.py
@@ -3,15 +3,36 @@ from calc import Skill_Damage
 from input import Skill_damage
 
 
-def Main_func():
-    try:
-        insntace = Skill_Damage(Skill_damage, 130)
-        D1 = insntace.D1()
-        D2 = insntace.D2(D1, 130)
-        print(D2)
+class Input:
+    @staticmethod
+    def Roll():
+        num = 0
+        try:
+            print("近接ですか？キャスターですか？ヒーラーですか？ m/c/h で入力してください。")
+            Roll_bool = input()
+            while True:
+                if Roll_bool == "m":
+                    insntace = Skill_Damage(Skill_damage, 100, 100)
+                    break
+                if Roll_bool == "c":
+                    insntace = Skill_Damage(Skill_damage, 130, 100)
+                    break
+                if Roll_bool == "h":
+                    insntace = Skill_Damage(Skill_damage, 130, 115)
+                    break
+                if num == 4:
+                    raise ValueError("error")
+                else:
+                    print("入力されいません。")
+                    print("近接ですか？キャスターですか？ヒーラーですか？ m/c/h で入力してください。")
+                    Roll_bool = input()
+                    num += 1
 
-    except ValueError as e:
-        print(e)
+            D1 = int(insntace.D1())
+            D2 = insntace.D2(D1)
+            print(D2)
+        except ValueError as e:
+            print(e)
 
 
-Main_func()
+Input.Roll()
